cc98011e2771a6f09f83524dde3875e919fe57d3
Luiz Ferreira <luiferreira@br0c02dx6jamd6n.ml.com>
2021-05-24 11:42:37 -0300

    finish ex1 aula 3

--- src/luiz/aula3/ex1/SortUtil.java
+++ src/luiz/aula3/ex1/SortUtil.java
@@ -1,7 +1,7 @@
 package luiz.aula3.ex1;
 
 public class SortUtil<T> {
-    public static <T extends Precedente> void sort(T[] arr) {
+    public static <T extends Precedente<T>> void sort(T[] arr) {
         for(int i = 0; i < arr.length - 1; i++) {
             for(int j = 0; j < arr.length - i -1; j++) {
                 if(arr[j].precedeA(arr[j+1]) > 0) {
@@ -12,7 +12,7 @@ public class SortUtil<T> {
 
     }
 
-    private static <T extends Precedente> void swap(int indexOne, int indexTwo, T[] arr) {
+    private static <T extends Precedente<T>> void swap(int indexOne, int indexTwo, T[] arr) {
         T temp = arr[indexOne];
         arr[indexOne] = arr[indexTwo];
         arr[indexTwo] = temp;
